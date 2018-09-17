ebd734dc1dac59134095edfd1eb0f33b610f1734
OHopiak <vorikus@gmail.com>
2018-09-17 13:36:34 +0300

    add deployment and theme-color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import logo from './assets/logo.svg';
 import {InfoBanner, Login, ResetPassword, SignUp, VerifyEmail,} from './components'
 import 'typeface-roboto'
